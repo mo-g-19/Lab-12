4bbdd4401bf19a7a4aefdfd75eb1347935de2cba
mo-g-19 <sggibson619@gmail.com>
2025-08-21 14:35:29 -0700

    A little formatting to make it easier to read

--- ways_to_load.py
+++ ways_to_load.py
@@ -7,9 +7,11 @@ Datasets useful experimenting algorithms and loaded as Bunch object
     (like dictionaries => data, target, and descriptive keys)
 """
 #Same example in the Getting Started Guide fro Scikit-Learn
-from sklearn.datasets import load_iris
+"""from sklearn.datasets import load_iris
 iris_data = load_iris()
 X, y = iris_data.data, iris_data.target
+"""
+
 
 """Method 2: fetch_* => loading from External Datasets
 Larger datasets/not inlcluded, fetch_* => retrieve data from internet
@@ -17,18 +19,22 @@ Larger datasets/not inlcluded, fetch_* => retrieve data from internet
 """
 #Accesses the MNIST dataset, large collection handwrtten digits => train image process systems
 #   Obtain data + target arrays ready preprocessing + model train
-from sklearn.datasets import fetch_openml
+"""from sklearn.datasets import fetch_openml
 mnist = fetch_openml('mnist_784', version=1)
 X, y = mnist.data, mnist.target
+"""
+
 
 """Method 3: load_svmlight_file => Importing for Sparse Data
 tailored loading datsets in SVMlight format (adv sparse datasets).
 Returns data and target suitable feeding directly estimator's fit() *fit funct mentioned get_start*
 """
 #Have a large and sparse dataset (SVM classifiers), read data + unnecessary increase mem usage (keep sparse format)
-from sklearn.datasets import load_svmlight_file
+"""from sklearn.datasets import load_svmlight_file
 X, y = load_svmlight_file('my_datset.txt')
+"""
 #>>Out: sparse matrix (X) and array target values (y)
+
 
 """Method 4: load_files => Loading Text Files
 loading txt organized into folders by class (common text classification tasks)
@@ -36,13 +42,16 @@ loading txt organized into folders by class (common text classification tasks)
 """
 #Particularly useful natural language processing task (category directory per class)
 #   Convinent load + vectorize textual data for classification alg
-from sklearn.datasets import load_files
+"""from sklearn.datasets import load_files
 text_data = load_files('txt_dataset/')
 X, y = text_data.data, text_data.target
+"""
+
 
 """*Bonus* Method 5: pandas.read_csbv => One-Liner load CSV
 not strictly scikit-learn funct, effective one-liner load CSV -> used scikit-learn ML models
 """
 #Importing CSV file into DataFrame, acquire versatile data structure ready preprocessing, exploration, + feeding into scikit models
-import pandas as pd
+"""import pandas as pd
 df = pd.rea_csv('my_data.csv')
+"""
